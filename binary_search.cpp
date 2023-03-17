d76ed73af9d632f6c650b66e5e8189cfdf6d20d7
MORENIKITA321 <127842598+MORENIKITA321@users.noreply.github.com>
2023-03-17 13:34:25 +0530

    Update binary_search.cpp

--- binary_search.cpp
+++ binary_search.cpp
@@ -3,7 +3,43 @@ using namespace std;
 
 int main()
 {
+  int n:
+  cout<<"enter a array size=>  ";
+  cin>>n
+    
+  int arr[n];
+  cout<<"enter"<<n<<"integers in assending order=> \n";
+  for(int i=0;i<n;i++)
+    cin>>arr[i];
   
-  cout<<"hello world";
+  
+  int target;
+  cout<<"enter the number you want to search=> ";
+  cin>>target:
+  
+  int start=0, end=n-1,mid;
+  int=pos=-1;
+  while(start<=end)
+  {
+    mid =(start+end)/2;
+    if(arr[mid]==target)
+    {
+      pos=mid;
+      break;
+    
+    }
+    else if(arr[mid]<target)
+            end = mid -1;   
+        else
+            start = mid + 1;
+  
+  
+  }
+   if(pos==-1)
+        cout<<target<<" is not present in the array"<<endl;
+    else
+        cout<<target<<" is present in the array at position "<<pos<<endl;
+        
+    return 0;
   
 }
